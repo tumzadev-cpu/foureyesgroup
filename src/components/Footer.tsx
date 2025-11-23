afffcc66c9c677a408534b0c00ebcaeed933d8b6
tumzadev-cpu <tumza.dev@gmail.com>
2025-11-23 12:32:54 +0200

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer({ onNavigate }: FooterProps) {
   };
 
   return (
-    <footer className="bg-white-900 text-white">
+    <footer className="bg-gray-100 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="col-span-1 md:col-span-2">
